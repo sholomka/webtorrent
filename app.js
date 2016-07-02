e29dfb68652e62d39d7c1a099857d66656d21993
sholomka <sholomka@gmail.com>
2016-07-02 18:50:03 +0300

    add files

---
+++ app.js
@@ -0,0 +1,16 @@
+class App {
+    constructor() {
+    }
+
+    static openTorrentFile(file) {
+
+
+    }
+
+
+    // var buildMagnetURI = function(infoHash) {
+    //     return 'magnet:?xt=urn:btih:' + infoHash + '&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969';
+    // }
+
+
+}
