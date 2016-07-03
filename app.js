3c861eb31a4acb1b51fd3d91db95daca3f937397
sholomka <sholomka@gmail.com>
2016-07-03 22:50:59 +0300

    add new logic

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 class App {
-    constructor() {
+    constructor() {}
+    
+    log (str) {
+        var p = document.createElement('p');
+        p.innerHTML = str;
+        document.querySelector('.log').appendChild(p)
     }
 
-    static openTorrentFile(file) {
+    seedFiles(file) {
+        client.seed(file, (torrent) => {
+            this.log('Got torrent metadata!');
+            this.log(
+                'Torrent info hash: ' + torrent.infoHash + ' ' +
+                '<a href="' + torrent.magnetURI + '" target="_blank">[Magnet URI]</a> ' +
+                '<a href="' + torrent.torrentFileBlobURL + '" target="_blank" download="' + torrent.name + '.torrent">[Download .torrent]</a>'
+            );
 
+            // Print out progress every 5 seconds
+            // var interval = setInterval(() => {
+            //     this.log('Progress: ' + (torrent.progress * 100).toFixed(1) + '%')
+            // }, 5000);
 
+            torrent.on('done', () => {
+                this.log('Progress: 100%');
+                // clearInterval(interval)
+            });
+
+            // Render all files into to the page
+            torrent.files.forEach(file => {
+                file.appendTo('.log');
+                this.log('(Blob URLs only work if the file is loaded from a server. "http//localhost" works. "file://" does not.)')
+                file.getBlobURL((err, url) => {
+                    if (err) return this.log(err.message);
+                    this.log('File done.');
+                    this.log('<a href="' + url + '" download="'+file.name+'">Download full file: ' + file.name + '</a>')
+                })
+            })
+        })        
     }
 
-
+    openTorrentFile(file) {
+;
+    }
+    
     // var buildMagnetURI = function(infoHash) {
     //     return 'magnet:?xt=urn:btih:' + infoHash + '&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969';
     // }
-
-
 }
---
+++ main.js
@@ -0,0 +1,23 @@
+var client = new WebTorrent();
+var app = new App();
+
+client.on('error', function (err) {
+    console.error('ERROR: ' + err.message)
+});
+
+
+
+document.querySelector('.seedFiles').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent page refresh
+
+//        var torrentId = document.querySelector('form input[name=torrentId]').value;
+    var torrentId = document.forms.upload.elements.myfile.files[0];
+
+    app.seedFiles(torrentId);
+
+    app.log('Adding ' + torrentId);
+//        client.add(torrentId, onTorrent)
+
+
+
+});
